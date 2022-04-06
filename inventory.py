162f956de197458809946410d0fc0bfe375c875e
Yuvneesh Kashyap <yuvneeshk53@gmail.com>
2022-04-06 00:41:47 -0400

    Creted home2 to work on

--- inventory.py
+++ inventory.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/home2')
+def home2():
+    return render_template("home2.html")  
+    
 if __name__ == "__main__":
     app.run()

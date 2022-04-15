02065fb2ee611e58c89e8e5f5475fce5e3fb7f0f
Yuvneesh Kashyap <yuvneeshk53@gmail.com>
2022-04-15 16:49:52 -0400

    Added validation to AddMaterials form

--- forms.py
+++ forms.py
@@ -5,6 +5,7 @@ from wtforms import StringField
 from wtforms import SelectField
 from wtforms import DateField
 from wtforms import SubmitField
+from wtforms.validators import DataRequired
 
 class ReceiveForm(FlaskForm):
     Product = SelectField(label='Product',choices=['','TAN3','TBN6'])
@@ -22,6 +23,6 @@ class WithdrawForm(FlaskForm):
     Certificate = SubmitField('Get Certificate')
 
 class AddMaterials(FlaskForm):
-    Product = StringField("Product")
-    CatalogNumber = StringField("Catalog Number")
+    Product = StringField("Product",validators=[DataRequired()])
+    CatalogNumber = StringField("Catalog Number",validators=[DataRequired()])
     add = SubmitField('Add')

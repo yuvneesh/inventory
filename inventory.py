a872dbf5c8998a3e16406d1458ee37355a1e40e5
Yuvneesh Kashyap <yuvneeshk53@gmail.com>
2022-04-23 14:17:32 -0400

    edited models import statement to import all(*)

--- inventory.py
+++ inventory.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from forms import ReceiveForm, WithdrawForm, AddMaterials
-from models import db,MaterialsList
+from models import *
 
 app = Flask(__name__)
 
